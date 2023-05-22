97ef64af3db1999d09359bb4515c5b8dc611708d
KangabireMuhozaMerveille <kangabirem@gmail.com>
2023-05-22 01:41:39 -0700

    prompt

--- prompt.c
+++ prompt.c
@@ -9,10 +9,11 @@ int main()
        int fd;
        const char* message = ":)";
        ssize_t bytes_written;
-       
+       char buffer[BUFFER_SIZE];
+
        fd = STDOUT_FILENO;
        
-	while (1);
+	while (1)
 	{
 		if (isatty(fd))
 		{
@@ -25,38 +26,41 @@ int main()
 			}
 			if (fgets(buffer, sizeof(buffer), stdin) != NULL)
 			{
-				if (strcmp(buffer,"exit\n") == 0 || strcmp(buffer, "quit/n") == 0)
+				if (strcmp(buffer,"exit\n") == 0 || strcmp(buffer, "quit\n") == 0)
 				{
 					memset(buffer, 0, sizeof(buffer));
+					break;
 				}
 			}
 
-{
-        char *line = NULL;
-        size_t capacity = 0;
-        ssize_t chars_read;
-        char buffer[BUFFER_SIZE];
-        ssize_t bytes_read;
-
-        while ((chars_read = getline(&line, &capacity, stdin)) != -1)
-       {	
-	       write(STDIN_FILENO, line, chars_read);
-	       
-       }
-        fseek(stdin, 0, SEEK_SET);
-
-
-        while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0)
-
-        {
-		write(STDIN_FILENO, buffer, bytes_read);
-		if (strcmp(line, "exit\n") == 0 || strcmp(line, "quit\n") == 0)
-		{
-			break;
 		}
-        }
+	}
+	{
+		char *line = NULL;
+		size_t capacity = 0;
+		ssize_t chars_read;
+		char buffer[BUFFER_SIZE];
+		ssize_t bytes_read;
 
-        free(line);
-}
-        return 0;
+		while ((chars_read = getline(&line, &capacity, stdin)) != -1)
+		{
+		write(STDIN_FILENO, line, chars_read);
+	       
+		}
+        	fseek(stdin, 0, SEEK_SET);
+
+
+	        while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0)
+
+        	{
+			write(STDIN_FILENO, buffer, bytes_read);
+			if (strcmp(line, "exit\n") == 0 || strcmp(line, "quit\n") == 0)
+			{
+				break;
+			}
+        	}
+
+		free(line);
+	}	
+		return 0;
 }
