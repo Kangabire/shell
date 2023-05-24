8286dfbb8af0bab625b9fbb3e6ea8d6f8851d106
martinsmind <martinsmind5@gmail.com>
2023-05-24 12:54:43 -0700

    simpleshell

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - copies a stri
+ *@dest: the destination stri to be copied into
+ *@src: the stri source
+ *@n: the amount of characters to be copied
+ *Return: the concatenated str
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strs
+ *@dest: the 1st str
+ *@src: the 2nd str
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated str
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a str
+ *@s: the str to be parsed
+ *@c: the character to look for
+ *Return: (s) the pointer to the memory area S
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
