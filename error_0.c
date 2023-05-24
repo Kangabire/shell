bfcb9d43750225cd800faebec3deeafcddf5bb43
martinsmind <martinsmind5@gmail.com>
2023-05-24 11:49:02 -0700

    simpleshell

---
+++ error_0.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ *_eputs - print an input str
+ * @str: the str to be printed
+ *
+ * Return: Absolutely Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - write character C to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1,
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - write character C to given fd
+ * @c: The character to print
+ * @fd: The file descriptor to write into
+ *
+ * Return: On success 1,
+ * On error, -1 is returned, and error is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - prints an input str
+ * @str: the str to be printed
+ * @fd: the file descriptor to write into
+ *
+ * Return: de number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
